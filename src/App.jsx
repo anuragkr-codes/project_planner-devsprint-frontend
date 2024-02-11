0cad94a01012896b2c2afc644fa2a65c13e36668
Akshat Sharan <akshat.sharan2004@gmail.com>
2024-02-11 12:23:47 +0530

    Adds auth component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Navbar/Navbar';
+import AuthComponent from './AuthComponent/AuthComponent';
 
 function App() {
   const [dataTheme, setDataTheme] = useState("dark");
@@ -12,6 +13,7 @@ function App() {
   return (
     <div id='app' data-theme={dataTheme}>
       <Navbar toggleAppearance={toggleAppearance} dataTheme={dataTheme} />
+      <AuthComponent />
       <div style={{height: '200vh', color: 'white'}}>Test Text</div>
     </div>
   )
