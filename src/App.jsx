bae539cb7fe00450e9f444b0846fc08818d8c712
Akshat Sharan <akshat.sharan2004@gmail.com>
2024-02-11 14:42:59 +0530

    Adds router and fixes spline position

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './LandingPage/LandingPage';
 
 function App() {
   return (
-    <div>
-      <LandingPage />
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './LandingPage/LandingPage';
+
+const Routes = () => {
+  return (
+    <Switch>
+    </Switch>
+  );
+};
+
+export default Routes;
